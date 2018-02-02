8dcd31f91ab4d2b6107d4dd8ebdc5640c9998934
RomainDW <romain.ollier34@gmail.com>
2018-02-02 17:24:03 +0100

    Config de la Database

---
+++ Engine/Config.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace TestProject\Engine;
+
+final class Config
+{
+    // Identifiants Database
+    const
+    DB_HOST = 'localhost',
+    DB_NAME = 'blog-ecrivain',
+    DB_USR = 'root',
+    DB_PWD = '',
+
+    // Titre du site
+    SITE_NAME = 'Mon Blog';
+}
