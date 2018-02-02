aeee0fb9095e185690067bb0bdb0daed6ee0eed8
RomainDW <romain.ollier34@gmail.com>
2018-02-02 17:31:58 +0100

    Création de la class Util qui sert a éviter la duplication de code

---
+++ Engine/Util.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace TestProject\Engine;
+
+class Util
+{
+    public function getView($sViewName)
+    {
+        $this->_get($sViewName, 'View');
+    }
+
+    public function getModel($sModelName)
+    {
+        $this->_get($sModelName, 'Model');
+    }
+
+    /**
+     * Cette méthode est utile pour éviter la duplication de code (crée presque la même méthode pour "getView" et "getModel")
+     */
+    private function _get($sFileName, $sType)
+    {
+        $sFullPath = ROOT_PATH . $sType . '/' . $sFileName . '.php';
+        if (is_file($sFullPath))
+            require $sFullPath;
+        else
+            exit('The "' . $sFullPath . '" file doesn\'t exist');
+    }
+
+    /**
+     * Défini les variables pour les template views.
+     */
+    public function __set($sKey, $mVal)
+    {
+        $this->$sKey = $mVal;
+    }
+}
