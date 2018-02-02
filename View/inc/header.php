5fef32836e1ba082fcb524b471376ff242bdd6bc
RomainDW <romain.ollier34@gmail.com>
2018-02-02 18:10:15 +0100

    création d'un template de header simple

---
+++ View/inc/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title><?=\BlogPhp\Engine\Config::SITE_NAME?></title>
+        <meta name="author" content="Romain Ollier" />
+        <link rel="stylesheet" href="<?=ROOT_URL?>static/style.css" />
+    </head>
+    <body>
+        <div class="center">
