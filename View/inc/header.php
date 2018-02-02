0ac098787bcddef7e8b3c0b8220c20957912422f
RomainDW <romain.ollier34@gmail.com>
2018-02-02 18:20:59 +0100

    ajout du js et css de materialize dans le header

--- View/inc/header.php
+++ View/inc/header.php
@@ -1,10 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <meta charset="utf-8" />
-        <title><?=\BlogPhp\Engine\Config::SITE_NAME?></title>
-        <meta name="author" content="Romain Ollier" />
-        <link rel="stylesheet" href="<?=ROOT_URL?>static/style.css" />
+      <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="<?=ROOT_URL?>static/css/materialize.css"  media="screen,projection"/>
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <meta charset="utf-8" />
+      <title><?=\BlogPhp\Engine\Config::SITE_NAME?></title>
+      <meta name="author" content="Romain Ollier" />
+
     </head>
     <body>
-        <div class="center">
+      <!--Import jQuery before materialize.js-->
+      <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+      <script type="text/javascript" src="<?=ROOT_URL?>static/js/materialize.js"></script>
+      <div class="center">
