48a090368b3cba3f2e5098b6850e860fb27c5dd3
RomainDW <romain.ollier34@gmail.com>
2018-02-03 17:13:55 +0100

    edit du lien vers la page d'accueil, ajout de méthodes pour éditer

--- Controller/Admin.php
+++ Controller/Admin.php
@@ -7,7 +7,7 @@ class Admin extends Blog
     public function login()
     {
         if ($this->isLogged())
-            header('Location: ' . ROOT_URL . '?p=blog&a=all');
+            header('Location: ' . ROOT_URL . '?p=blog&a=index');
 
         if (isset($_POST['email'], $_POST['password']))
         {
@@ -18,7 +18,7 @@ class Admin extends Blog
             if (password_verify($_POST['password'], $sHashPassword))
             {
                 $_SESSION['is_logged'] = 1; // Admin est connecté maintenant
-                header('Location: ' . ROOT_URL . '?p=blog&a=all');
+                header('Location: ' . ROOT_URL . '?p=blog&a=index');
                 exit;
             }
             else
@@ -46,5 +46,48 @@ class Admin extends Blog
         // Redirection à la page d'accueil
         header('Location: ' . ROOT_URL);
         exit;
+    }
+
+    public function edit()
+    {
+      if (!$this->isLogged())
+      header('Location: ?p=blog&a=index');
+
+      $this->oUtil->oPosts = $this->oModel->getAll();
+      $this->oUtil->getView('edit');
+    }
+
+    public function editPost()
+    {
+      if (!$this->isLogged())
+      header('Location: ?p=blog&a=index');
+
+      if (isset($_POST['edit_submit']))
+      {
+        if (empty($_POST['title']) || empty($_POST['body']))
+        {
+          $this->oUtil->sErrMsg = 'Tous les champs doivent être remplis.';
+        }
+        else
+        {
+          $aData = array('post_id' => $_GET['id'], 'title' => $_POST['title'], 'body' => $_POST['body']);
+          $this->oModel->update($aData);
+          $this->oUtil->sSuccMsg = 'L\'article a bien été mis à jour !';
+
+        }
+      }
+
+      /* Récupère les données du post */
+      $this->oUtil->oPost = $this->oModel->getById($_GET['id']);
+
+      $this->oUtil->getView('edit_post');
+    }
+
+    public function delete()
+    {
+      if (!$this->isLogged()) exit;
+
+      $this->oModel->delete($_GET['id']);
+      header('Location: ?p=admin&a=edit');
     }
 }
