2b07c7e381cb10e6dd69368473c7db7f3b394219
RomainDW <romain.ollier34@gmail.com>
2018-02-03 19:38:08 +0100

    Création de la page add_post qui permet d'ajouter un post

---
+++ View/add_post.php
@@ -0,0 +1,38 @@
+<?php require 'inc/header.php' ?>
+<?php require 'inc/topbar.php' ?>
+
+<div class="container">
+  <h1>Poster un article</h1>
+  <?php require 'inc/msg.php' ?>
+  <form method="post" enctype="multipart/form-data">
+	<div class="row">
+		<div class="input-field col s12">
+			<input type="text" name="title" id="title" required="required">
+			<label for="title">Titre de l'article</label>
+		</div>
+
+		<div class="input-field col s12">
+			<textarea name="body" id="body" class="materialize-textarea" required="required"></textarea>
+			<label for="body">Contenu de l'article</label>
+		</div>
+
+		<div class="col s12">
+			<div class="input-field file-field">
+				<div class="btn">
+					<span>Image de l'article</span>
+					<input type="file" name="image">
+				</div>
+				<div class="file-path-wrapper">
+					<input type="text" class="file-path validate" readonly>
+				</div>
+			</div>
+		</div>
+
+		<div class="col s12 right-align">
+			<br><br>
+			<button class="btn" type="submit" name="add_submit">Publier</button>
+		</div>
+
+	</div>
+</form>
+</div>
