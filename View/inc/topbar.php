284de730a5c250565c8e2bb1310db24d8871f022
RomainDW <romain.ollier34@gmail.com>
2018-02-03 12:36:42 +0100

    ajout de navbar-fixed

--- View/inc/topbar.php
+++ View/inc/topbar.php
@@ -1,20 +1,22 @@
-<nav class="light-blue">
-	<div class="container">
-		<div class="nav-wrapper">
-			<a href="<?=ROOT_URL?>?p=blog&amp;a=index" class="brand-logo">Billet simple pour l'Alaska</a>
+<div class="navbar-fixed">
+	<nav class="light-blue">
+		<div class="container">
+			<div class="nav-wrapper">
+				<a href="<?=ROOT_URL?>?p=blog&amp;a=index" class="brand-logo">Billet simple pour l'Alaska</a>
 
-			<a href="#" data-activates="mobile-menu" class="button-collapse"><i class="material-icons">menu</i></a>
+				<a href="#" data-activates="mobile-menu" class="button-collapse"><i class="material-icons">menu</i></a>
 
-			<ul class="right hide-on-med-and-down">
-				<li class="<?php echo ($_GET['a']=="index")?"active" : ""; ?>"><a href="<?=ROOT_URL?>?p=blog&amp;a=index">Accueil</a></li>
-				<li class="<?php echo ($_GET['a']=="chapters")?"active" : ""; ?>"><a href="<?=ROOT_URL?>?p=blog&amp;a=chapters">Chapitres</a></li>
-			</ul>
+				<ul class="right hide-on-med-and-down">
+					<li class="<?php echo ($_GET['a']=="index")?"active" : ""; ?>"><a href="<?=ROOT_URL?>?p=blog&amp;a=index">Accueil</a></li>
+					<li class="<?php echo ($_GET['a']=="chapters")?"active" : ""; ?>"><a href="<?=ROOT_URL?>?p=blog&amp;a=chapters">Chapitres</a></li>
+				</ul>
 
-			<ul class="side-nav" id="mobile-menu">
-				<li class="<?php echo ($_GET['a']=="index")?"active" : ""; ?>"><a href="<?=ROOT_URL?>">Accueil</a></li>
-				<li class="<?php echo ($_GET['a']=="chapters")?"active" : ""; ?>"><a href="<?=ROOT_URL?>?p=blog&amp;a=chapters">Chapitres</a></li>
-			</ul>
+				<ul class="side-nav" id="mobile-menu">
+					<li class="<?php echo ($_GET['a']=="index")?"active" : ""; ?>"><a href="<?=ROOT_URL?>">Accueil</a></li>
+					<li class="<?php echo ($_GET['a']=="chapters")?"active" : ""; ?>"><a href="<?=ROOT_URL?>?p=blog&amp;a=chapters">Chapitres</a></li>
+				</ul>
 
+			</div>
 		</div>
-	</div>
-</nav>
+	</nav>
+</div>
