ddd17fb4d541f010813c6607ace0a8b861aec6f2
RomainDW <romain.ollier34@gmail.com>
2018-02-03 14:37:44 +0100

    Ajout de la page login pour se connecter à l'admin

---
+++ View/login.php
@@ -0,0 +1,41 @@
+<?php require 'inc/header.php' ?>
+<?php require 'inc/topbar.php' ?>
+<div class="container">
+  <div class="row">
+  	<div class="col l4 m6 s12 offset-l4 offset-m3">
+  		<div class="card-panel">
+  			<div class="row">
+  				<div class="col s6 offset-s3">
+  					<img src="static/img/admin.png" alt="Administrator" width="100%">
+  				</div>
+  			</div>
+
+  			<h4 class="center-align">Se connecter</h4>
+
+        <?php require 'inc/msg.php' ?>
+
+  			<form method="post">
+  				<div class="row">
+  					<div class="input-field col s12">
+  						<input type="email" id="email" name="email" required="required">
+  						<label for="email">Adresse email</label>
+  					</div>
+
+  					<div class="input-field col s12">
+  						<input type="password" id="password" name="password" required="required">
+  						<label for="password">Mot de passe</label>
+  					</div>
+  				</div>
+
+  				<center>
+  					<button type="submit" name="submit" class="waves-effect waves-light btn light-blue">
+  						<i class="material-icons left">perm_identity</i>
+  						Se connecter
+  					</button>
+  				</center>
+  			</form>
+
+  		</div>
+  	</div>
+  </div>
+</div>
