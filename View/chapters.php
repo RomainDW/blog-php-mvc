e24c0e9adfa9345af6caffe51f3f4647dcddff7b
RomainDW <romain.ollier34@gmail.com>
2018-02-03 19:39:12 +0100

    Modification du nommage de la variable $oChapters en $oPosts

--- View/chapters.php
+++ View/chapters.php
@@ -3,18 +3,20 @@
 
 <div class="container">
   <h2>Liste des chapitres</h2>
-  <?php foreach ($this->oChapters as $oChapter): ?>
+  <?php foreach ($this->oPosts as $oPost): ?>
     <div class="row">
 			<div class="col s12 m12 l12">
-				<h4><?= $oChapter->title ?></h4>
+				<h4><?= $oPost->title ?></h4>
 				<div class="row">
 					<div class="col s12 m6 l8">
-						<?= nl2br(htmlspecialchars(mb_strimwidth($oChapter->body, 0, 1200, '...'))) ?>...
+						<?= nl2br(htmlspecialchars(mb_strimwidth($oPost->body, 0, 1200, '...'))) ?>...
+            <br><br>
+            <?php require 'inc/control_buttons.php' ?>
 					</div>
 					<div class="col s12 m6 l4">
-						<img src="<?=ROOT_URL?>static/img/posts/<?= $oChapter->image ?>" class="materialboxed responsive-img" alt="<?= $oChapter->title ?>"/>
+						<img src="<?=ROOT_URL?>static/img/posts/<?= $oPost->image ?>" class="materialboxed responsive-img" alt="<?= $oPost->title ?>"/>
 						<br/><br/>
-				  	<a class="btn light-blue waves-effect waves-light" href="<?=ROOT_URL?>?p=blog&amp;a=post&amp;id=<?=$oChapter->id?>">Lire le chapitre au complet</a>
+				  	<a class="btn light-blue waves-effect waves-light" href="<?=ROOT_URL?>?p=blog&amp;a=post&amp;id=<?=$oPost->id?>">Lire le chapitre au complet</a>
 					</div>
 				</div>
 			</div>
