2fbafdaedb6aa6870ed8175a59594d08c8a9ccaf
RomainDW <romain.ollier34@gmail.com>
2018-02-04 22:16:15 +0100

    Ajout d'un preg_replace pour ne pas afficher les balises img

--- View/chapters.php
+++ View/chapters.php
@@ -5,11 +5,13 @@
   <h2>Liste des chapitres</h2>
   <?php foreach ($this->oPosts as $oPost): ?>
     <div class="row">
+      <hr>
 			<div class="col s12 m12 l12">
 				<h4><?= $oPost->title ?></h4>
 				<div class="row">
 					<div class="col s12 m6 l8">
-						<?= nl2br(htmlspecialchars(mb_strimwidth($oPost->body, 0, 1200, '...'))) ?>...
+            <!-- On affiche les 1200 premiers caractères et on affiche pas les images -->
+						<?= preg_replace("/<img[^>]+\>/i", "", nl2br(mb_strimwidth($oPost->body, 0, 800, '...'))); ?>
             <br><br>
             <?php require 'inc/control_buttons.php' ?>
 					</div>
