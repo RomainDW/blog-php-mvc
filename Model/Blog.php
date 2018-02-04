07cfb14352b81c235bcecbbeb2b6758bf777147f
RomainDW <romain.ollier34@gmail.com>
2018-02-04 22:17:02 +0100

    Déplacement des méthodes d'admin dans le model Admin

--- Model/Blog.php
+++ Model/Blog.php
@@ -45,49 +45,4 @@ class Blog
     $oStmt = $this->oDb->query('SELECT * FROM Posts ORDER BY createdDate DESC');
     return $oStmt->fetchAll(\PDO::FETCH_OBJ);
   }
-
-  public function delete($iId)
-  {
-    $oStmt = $this->oDb->prepare('DELETE FROM Posts WHERE id = :postId LIMIT 1');
-    $oStmt->bindParam(':postId', $iId, \PDO::PARAM_INT);
-    return $oStmt->execute();
-  }
-
-  public function update(array $aData)
-  {
-    $oStmt = $this->oDb->prepare('UPDATE Posts SET title = :title, body = :body WHERE id = :postId LIMIT 1');
-    $oStmt->bindValue(':postId', $aData['post_id'], \PDO::PARAM_INT);
-    $oStmt->bindValue(':title', $aData['title']);
-    $oStmt->bindValue(':body', $aData['body']);
-    return $oStmt->execute();
-  }
-
-  public function add(array $aData)
-  {
-    $oStmt = $this->oDb->prepare('INSERT INTO Posts (title, body, createdDate) VALUES(:title, :body, :created_date)');
-    return $oStmt->execute($aData);
-  }
-
-  public function postImg($tmp_name, $extension){
-    $i = [
-      'id'     => $this->oDb->lastInsertId(),
-      'image'  => $this->oDb->lastInsertId().$extension
-    ];
-
-    $oStmt = $this->oDb->prepare('UPDATE Posts SET image = :image WHERE id = :id');
-    move_uploaded_file($tmp_name,"static/img/posts/".$i['image']);
-    return $oStmt->execute($i);
-  }
-
-  public function updateImg($name, $id, $tmp_name)
-  {
-    $i = [
-      'id'     => $id,
-      'image'  => $name
-    ];
-
-    $oStmt = $this->oDb->prepare('UPDATE Posts SET image = :image WHERE id = :id');
-    move_uploaded_file($tmp_name,"static/img/posts/".$i['image']);
-    return $oStmt->execute($i);
-  }
 }
