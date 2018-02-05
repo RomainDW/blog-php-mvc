f2e39452d5a167f0d53ed476d63fff98e4e8d31a
RomainDW <romain.ollier34@gmail.com>
2018-02-05 17:14:42 +0100

    Ajout lien vers connexion

--- View/inc/topbar.php
+++ View/inc/topbar.php
@@ -12,15 +12,34 @@
 					<li class="<?php echo ($_GET['a']=="index")?"active" : ""; ?>"><a href="<?=ROOT_URL?>?p=blog&amp;a=index">Accueil</a></li>
 					<li class="<?php echo ($_GET['a']=="chapters")?"active" : ""; ?>"><a href="<?=ROOT_URL?>?p=blog&amp;a=chapters">Chapitres</a></li>
 
-					<?php if (!empty($_SESSION['is_logged'])): ?>
+					<?php if (empty($_SESSION['is_admin']) && empty($_SESSION['is_logged'])): ?>
+					<li class="<?php echo ($_GET['a']=="login")?"active" : ""; ?>"><a href="<?=ROOT_URL?>?p=blog&amp;a=login"><i class="material-icons">lock_open</i></a></li>
+					<?php endif ?>
+
+					<?php if (!empty($_SESSION['is_admin'])): ?>
 					<li class="<?php echo ($_GET['a']=="edit")?"active" : ""; ?>"><a href="<?=ROOT_URL?>?p=admin&amp;a=edit"><i class="material-icons">edit</i></a></li>
-					<li><a href="<?=ROOT_URL?>?p=admin&amp;a=logout">Déconnexion</a></li>
+					<?php endif ?>
+
+					<?php if (!empty($_SESSION['is_admin']) || !empty($_SESSION['is_logged'])): ?>
+					<li><a href="<?=ROOT_URL?>?p=blog&amp;a=logout">Déconnexion</a></li>
 					<?php endif ?>
 				</ul>
 
 				<ul class="side-nav" id="mobile-menu">
 					<li class="<?php echo ($_GET['a']=="index")?"active" : ""; ?>"><a href="<?=ROOT_URL?>">Accueil</a></li>
 					<li class="<?php echo ($_GET['a']=="chapters")?"active" : ""; ?>"><a href="<?=ROOT_URL?>?p=blog&amp;a=chapters">Chapitres</a></li>
+
+					<?php if (empty($_SESSION['is_admin']) || empty($_SESSION['is_logged'])): ?>
+					<li class="<?php echo ($_GET['a']=="login")?"active" : ""; ?>"><a href="<?=ROOT_URL?>?p=blog&amp;a=login">Connexion</a></li>
+					<?php endif ?>
+
+					<?php if (!empty($_SESSION['is_admin'])): ?>
+					<li class="<?php echo ($_GET['a']=="edit")?"active" : ""; ?>"><a href="<?=ROOT_URL?>?p=admin&amp;a=edit">edition</a></li>
+					<?php endif ?>
+
+					<?php if (!empty($_SESSION['is_admin']) || !empty($_SESSION['is_logged'])): ?>
+					<li><a href="<?=ROOT_URL?>?p=blog&amp;a=logout">Déconnexion</a></li>
+					<?php endif ?>
 				</ul>
 
 			</div>
