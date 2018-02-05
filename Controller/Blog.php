26865b1850afeca87ed44965d5bc382e1ee00b69
RomainDW <romain.ollier34@gmail.com>
2018-02-05 17:09:54 +0100

    Ajout méthode registration, mtn le pseudo de commentaire est celui de l'utilisateur

--- Controller/Blog.php
+++ Controller/Blog.php
@@ -50,18 +50,19 @@ class Blog
     {
       header('Location: ?p=blog&a=index');
     }
+
     $this->oUtil->oPost = $this->oModel->getById($this->_iId); // Récupère les données du post
 	  $this->oUtil->oComments = $this->oModel->getComments();
 
   	if (isset($_POST['submit_comment']))
     {
-        if (empty($_POST['name']) || empty($_POST['comment']))
+        if (empty($_POST['comment']))
         {
-          $this->oUtil->sErrMsg = 'Tous les champs n\'ont pas été remplis';
+          $this->oUtil->sErrMsg = 'Vous n\'avez pas écrit de commentaire';
         }
         else
         {
-          $aData = array('name' => $_POST['name'], 'comment' => $_POST['comment'], 'post_id' => $_GET['id']);
+          $aData = array('name' => current($_SESSION), 'comment' => $_POST['comment'], 'post_id' => $_GET['id']);
           $this->oModel->addComment($aData);
           ?> <script>window.location.replace('?p=blog&a=post&id=<?= $_GET['id'] ?>');</script> <?php
           $this->oUtil->sSuccMsg = 'Le Commentaire a été posté !';
@@ -77,9 +78,106 @@ class Blog
     $this->oUtil->getView('chapters');
   }
 
-  /***** For Admin (Back end) *****/
   protected function isLogged()
   {
-    return !empty($_SESSION['is_logged']);
+    return !empty($_SESSION['is_admin']); // si admin est connecté return true
   }
+
+	public function login()
+	{
+			if ($this->isLogged())
+					header('Location: ' . ROOT_URL . '?p=blog&a=index');
+
+			if (isset($_POST['submit']))
+			{
+				$sEmail = htmlspecialchars(trim($_POST['email']));
+				$sPassword = htmlspecialchars(trim($_POST['password']));
+				$oIsAdmin = $this->oModel->isAdmin($_POST['email']);
+
+				if (empty($sEmail) || empty($sPassword))
+				{
+					$this->oUtil->sErrMsg = "Tous les champs n'ont pas été remplis !";
+				}
+				elseif($this->oModel->login($sEmail, $sPassword) == 0)
+				{
+					$this->oUtil->sErrMsg = "Identifiant ou mot de passe incorrect!";
+				}
+				else
+				{
+					if ($oIsAdmin->admin != null)
+					{
+						$_SESSION['is_admin'] = $oIsAdmin->pseudo; // Admin est connecté maintenant
+						header('Location: ' . ROOT_URL . '?p=blog&a=index');
+						exit;
+					}
+					else
+					{
+						$_SESSION['is_logged'] = $oIsAdmin->pseudo; // user est connecté maintenant
+						header('Location: ' . ROOT_URL . '?p=blog&a=index');
+						exit;
+					}
+				}
+			}
+
+			$this->oUtil->getView('login');
+	}
+
+	public function logout()
+	{
+		if (!$this->isLogged())
+			header('Location: ?p=blog&a=index');
+
+		// Si il y a une session, la détruit pour déconnecter l'admin
+		if (!empty($_SESSION))
+		{
+			$_SESSION = array();
+			session_unset();
+			session_destroy();
+		}
+
+		// Redirection à la page d'accueil
+		header('Location: ' . ROOT_URL);
+		exit;
+	}
+
+	public function registration()
+	{
+		if ($this->isLogged())
+			header('Location: ?p=blog&a=index');
+
+		if (isset($_POST['submit']))
+		{
+			$sPassword = htmlspecialchars(trim($_POST['password']));
+      $sPassword_again = htmlspecialchars(trim($_POST['password_again']));
+			$sEmail = htmlspecialchars(trim($_POST['email']));
+			$sPseudo = htmlspecialchars(trim($_POST['pseudo']));
+
+			if (empty($sPassword) || empty($sPassword_again))
+			{
+				$this->oUtil->sErrMsg = "Tous les champs n'ont pas été remplis";
+			}
+
+			elseif ($sPassword != $sPassword_again)
+			{
+				$this->oUtil->sErrMsg = "Les mots de passe sont différents";
+			}
+
+			else
+			{
+				$aData = array('email' => $sEmail, 'pseudo' => $sPseudo, 'password' => sha1($sPassword));
+				$this->oModel->addUser($aData);
+				?> <script>window.location.replace('?p=blog&a=login');</script> <?php
+				$this->oUtil->sSuccMsg = 'Votre compte a été créé, vous pouvez maintenant vous connecter';
+			}
+
+		}
+
+		$this->oUtil->getView('registration');
+	}
+
+	public function signal()
+	{
+
+		
+	}
 }
