b4d0b0fdc5d17a4f0e25b958e0deabaaa78cca38
RomainDW <romain.ollier34@gmail.com>
2018-02-05 17:16:06 +0100

    Changement de couleur du btn supprimer : rouge

--- View/inc/control_buttons.php
+++ View/inc/control_buttons.php
@@ -1,6 +1,6 @@
-<?php if(!empty($_SESSION['is_logged'])): ?>
+<?php if(!empty($_SESSION['is_admin'])): ?>
 
   <a href="<?=ROOT_URL?>?p=admin&amp;a=editPost&amp;id=<?=$oPost->id?>"><button class="btn light-blue">Modifier</button></a>
-  <a href="<?=ROOT_URL?>?p=admin&amp;a=delete&amp;id=<?=$oPost->id?>"><button class="btn light-blue">Supprimer</button></a>
+  <a href="<?=ROOT_URL?>?p=admin&amp;a=delete&amp;id=<?=$oPost->id?>"><button class="btn red">Supprimer</button></a>
 
 <?php endif ?>
