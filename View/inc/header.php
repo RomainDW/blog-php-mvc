4caa308078d9c9897d3a726875a9095e03d63fd1
RomainDW <romain.ollier34@gmail.com>
2018-02-06 21:06:55 +0100

    ajout du lien vers style.css

--- View/inc/header.php
+++ View/inc/header.php
@@ -5,7 +5,7 @@
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <!--Import materialize.css-->
       <link type="text/css" rel="stylesheet" href="<?=ROOT_URL?>static/css/materialize.css"  media="screen,projection"/>
-
+      <link type="text/css" rel="stylesheet" href="<?=ROOT_URL?>static/css/style.css"/>
       <!--Let browser know website is optimized for mobile-->
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       <meta charset="utf-8" />
