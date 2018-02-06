c625bd78fc123786b9eb8ee5c5b5187ad2f0d729
RomainDW <romain.ollier34@gmail.com>
2018-02-06 21:08:59 +0100

    ajout des méthodes pour le dashboard

--- Model/Admin.php
+++ Model/Admin.php
@@ -36,6 +36,11 @@ class Admin extends Blog
       $oStmt->bindParam(':postId', $iId, \PDO::PARAM_INT);
       return $oStmt->execute();
     }
+    public function deleteVotes($iId){
+      $oStmt = $this->oDb->prepare('DELETE FROM Votes WHERE post_id = :postId');
+      $oStmt->bindParam(':postId', $iId, \PDO::PARAM_INT);
+      return $oStmt->execute();
+    }
 
     public function add(array $aData)
     {
@@ -62,5 +67,54 @@ class Admin extends Blog
       return $oStmt->execute();
     }
 
+    public function deleteVote($iId)
+    {
+      $oStmt = $this->oDb->prepare('DELETE FROM Votes WHERE comment_id = :comment_id');
+      $oStmt->bindParam(':comment_id', $iId, \PDO::PARAM_INT);
+      return $oStmt->execute();
+    }
+
+    public function inTable($sTable)
+    {
+      $oStmt = $this->oDb->query("SELECT COUNT(id) FROM $sTable");
+      return $oStmt->fetch();
+    }
+
+    public function getCommentsUnseen()
+    {
+      $oStmt = $this->oDb->query("
+        SELECT  comments.id,
+                comments.name,
+                comments.comment,
+                comments.post_id,
+                comments.date,
+                comments.signals,
+                Posts.title
+        FROM comments
+        JOIN posts
+        ON comments.post_id = Posts.id
+        WHERE comments.seen = '0'
+        ORDER BY comments.date ASC
+      ");
+
+      $results = [];
+
+      while($rows = $oStmt->fetchObject())
+      {
+        $results[] = $rows;
+      }
+      return $results;
+    }
+
+    public function see_comment()
+    {
+      $oStmt = $this->oDb->exec("UPDATE comments SET seen='1' WHERE id='{$_POST['id']}'");
+    }
+
+    public function delete_comment()
+    {
+      $oStmt = $this->oDb->exec("DELETE FROM comments WHERE id = {$_POST['id']}");
+      $oStmt = $this->oDb->exec("DELETE FROM Votes WHERE comment_id = {$_POST['id']}");
+    }
 
 }
