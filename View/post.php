039021367d1635215e02e9ca465aa7b1843e9b2f
RomainDW <romain.ollier34@gmail.com>
2018-02-06 21:05:25 +0100

    ajout de la colloration du btn signaler

--- View/post.php
+++ View/post.php
@@ -20,12 +20,13 @@
 
 <!-- Commentaires -->
 
-	<h4>Commentaires :</h4>
+	<h4 id="comment_ink">Commentaires :</h4>
 	<?php if (empty($this->oComments)): ?>
 	<p class="bold">Aucun commentaire n'a été publié... Soyez le premier!</p>
 	<?php else: ?>
 
 	<?php foreach ($this->oComments as $oComment): ?>
+
     <blockquote id="blockquote">
       <strong><?= $oComment->name ?> <em>(Le <?= date('d/m/Y', strtotime($oComment->date)) ?>)</em></strong>
       <p><?= nl2br($oComment->comment); ?></p>
@@ -33,7 +34,19 @@
     <?php if (!empty($_SESSION['is_admin'])): ?>
       <a href="<?=ROOT_URL?>?p=admin&amp;a=deleteComment&amp;id=<?=$oComment->id?>&amp;postid=<?=$this->oPost->id?>"><button class="btn red">Supprimer</button></a>
     <?php endif ?>
-    
+
+    <?php if(!empty($_SESSION['is_logged'])): ?>
+      <?php $aIsSignaled = array(); ?>
+      <?php foreach($this->oUserVotes as $key => $userVote): ?>
+        <?php $aIsSignaled[] = $userVote->comment_id; ?>
+      <?php endforeach ?>
+      <?php if(!in_array($oComment->id ,$aIsSignaled)): ?>
+        <?php $this->color = '' ;?>
+      <?php endif ?>
+      <form class="vote-form" action="?p=blog&a=signal&postid=<?=$this->oPost->id?>&commentId=<?=$oComment->id?>&vote=1" method="POST">
+        <button class="btn red signal-btn <?= $this->color ?>" type="submit">Signaler</button>
+      </form>
+    <?php endif ?>
 
 	<?php endforeach ?>
 	<?php endif ?>
