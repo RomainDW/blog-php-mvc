3782293e27b24fa527c3668c521addab1f8819be
RomainDW <romain.ollier34@gmail.com>
2018-02-06 20:42:59 +0100

    ajout d'un script pour boite modal du dashboard

--- static/js/script.js
+++ static/js/script.js
@@ -3,4 +3,20 @@ $(document).ready(function(){
 	$('.button-collapse').sideNav();
 	$('select').material_select();
 
+	$('.modal').modal();
+
+	$('.see_comment').click(function(){
+		var id = $(this).attr('id');
+		$.post('?p=admin&a=see_comment', {id:id}, function(){
+			$('#commentaire_'+id).hide();
+		});
+	});
+
+	$('.delete_comment').click(function(){
+		var id = $(this).attr('id');
+		$.post('?p=admin&a=delete_comment', {id:id}, function(){
+			$('#commentaire_'+id).hide();
+		});
+	});
+
 });
