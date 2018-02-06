3529111ea80e31acdb649197d58eee131cd028f9
RomainDW <romain.ollier34@gmail.com>
2018-02-06 21:06:28 +0100

    ajout des liens vers les nouvelles pages

--- View/inc/topbar.php
+++ View/inc/topbar.php
@@ -9,11 +9,16 @@
 				<a href="#" data-activates="mobile-menu" class="button-collapse"><i class="material-icons">menu</i></a>
 
 				<ul class="right hide-on-med-and-down">
+
 					<li class="<?php echo ($_GET['a']=="index")?"active" : ""; ?>"><a href="<?=ROOT_URL?>?p=blog&amp;a=index">Accueil</a></li>
 					<li class="<?php echo ($_GET['a']=="chapters")?"active" : ""; ?>"><a href="<?=ROOT_URL?>?p=blog&amp;a=chapters">Chapitres</a></li>
 
 					<?php if (empty($_SESSION['is_admin']) && empty($_SESSION['is_logged'])): ?>
-					<li class="<?php echo ($_GET['a']=="login")?"active" : ""; ?>"><a href="<?=ROOT_URL?>?p=blog&amp;a=login"><i class="material-icons">lock_open</i></a></li>
+					<li><a href="<?=ROOT_URL?>?p=blog&amp;a=login" class="btn green waves-effect waves-light">Connexion<i class="material-icons right">lock_open</i></a></li>
+					<?php endif ?>
+
+					<?php if (!empty($_SESSION['is_admin'])): ?>
+					<li class="<?php echo ($_GET['a']=="dashboard")?"active" : ""; ?>"><a href="<?=ROOT_URL?>?p=admin&amp;a=dashboard"><i class="material-icons">dashboard</i></a></li>
 					<?php endif ?>
 
 					<?php if (!empty($_SESSION['is_admin'])): ?>
@@ -21,11 +26,12 @@
 					<?php endif ?>
 
 					<?php if (!empty($_SESSION['is_admin']) || !empty($_SESSION['is_logged'])): ?>
-					<li><a href="<?=ROOT_URL?>?p=blog&amp;a=logout">Déconnexion</a></li>
+					<li><a href="<?=ROOT_URL?>?p=blog&amp;a=logout" class="btn red">Déconnexion<i class="material-icons right">lock_outline</i></a></li>
 					<?php endif ?>
 				</ul>
 
 				<ul class="side-nav" id="mobile-menu">
+
 					<li class="<?php echo ($_GET['a']=="index")?"active" : ""; ?>"><a href="<?=ROOT_URL?>">Accueil</a></li>
 					<li class="<?php echo ($_GET['a']=="chapters")?"active" : ""; ?>"><a href="<?=ROOT_URL?>?p=blog&amp;a=chapters">Chapitres</a></li>
 
@@ -34,7 +40,11 @@
 					<?php endif ?>
 
 					<?php if (!empty($_SESSION['is_admin'])): ?>
-					<li class="<?php echo ($_GET['a']=="edit")?"active" : ""; ?>"><a href="<?=ROOT_URL?>?p=admin&amp;a=edit">edition</a></li>
+					<li class="<?php echo ($_GET['a']=="dashboard")?"active" : ""; ?>"><a href="<?=ROOT_URL?>?p=admin&amp;a=dashboard">Dashboard</a></li>
+					<?php endif ?>
+
+					<?php if (!empty($_SESSION['is_admin'])): ?>
+					<li class="<?php echo ($_GET['a']=="edit")?"active" : ""; ?>"><a href="<?=ROOT_URL?>?p=admin&amp;a=edit">Edition</a></li>
 					<?php endif ?>
 
 					<?php if (!empty($_SESSION['is_admin']) || !empty($_SESSION['is_logged'])): ?>
