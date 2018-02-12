eba225643e6056f01fd67cb9f86b3a801079ab98
RomainDW <romain.ollier34@gmail.com>
2018-02-12 18:50:13 +0100

    Ajout d'un footer

---
+++ View/inc/footer.php
@@ -0,0 +1,17 @@
+
+
+
+<footer class="page-footer light-blue">
+  <div class="footer-copyright">
+    <div class="container">
+      <div class="row footer-row">
+        <div class="col l6 s12">
+          Copyright © <?= date("Y"); ?> Jean Forteroche
+        </div>
+        <div class="col l4 offset-l2 s12">
+          <a href="<?=ROOT_URL?>?p=blog&amp;a=legalNotice" class="white-text">Mentions légales</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
