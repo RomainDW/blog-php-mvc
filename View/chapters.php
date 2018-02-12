8fba6410836a0f2e3a446efe52834c9f65c18b09
RomainDW <romain.ollier34@gmail.com>
2018-02-12 18:48:26 +0100

    Ajout du footer et modification par rapport a Users id

--- View/chapters.php
+++ View/chapters.php
@@ -1,27 +1,29 @@
 <?php require 'inc/header.php' ?>
 <?php require 'inc/topbar.php' ?>
-
-<div class="container">
-  <h2>Liste des chapitres</h2>
-  <?php foreach ($this->oPosts as $oPost): ?>
-    <div class="row">
-      <hr>
-			<div class="col s12 m12 l12">
-				<h4><?= $oPost->title ?></h4>
-				<div class="row">
-					<div class="col s12 m6 l8">
-            <!-- On affiche les 1200 premiers caractères et on affiche pas les images -->
-						<?= preg_replace("/<img[^>]+\>/i", "", nl2br(mb_strimwidth($oPost->body, 0, 800, '...'))); ?>
-            <br><br>
-            <?php require 'inc/control_buttons.php' ?>
-					</div>
-					<div class="col s12 m6 l4">
-						<img src="<?=ROOT_URL?>static/img/posts/<?= $oPost->image ?>" class="materialboxed responsive-img" alt="<?= $oPost->title ?>"/>
-						<br/><br/>
-				  	<a class="btn light-blue waves-effect waves-light" href="<?=ROOT_URL?>?p=blog&amp;a=post&amp;id=<?=$oPost->id?>">Lire le chapitre</a>
-					</div>
-				</div>
-			</div>
-		</div>
-  <?php endforeach ?>
-</div>
+<main>
+  <div class="container">
+    <h1>Liste des chapitres</h1>
+    <?php foreach ($this->oPosts as $oPost): ?>
+      <div class="row">
+        <hr>
+  			<div class="col s12 m12 l12">
+  				<h4><?= $oPost->title ?></h4>
+  				<div class="row">
+  					<div class="col s12 m6 l8">
+              <!-- On affiche les 1200 premiers caractères et on affiche pas les images -->
+  						<?= preg_replace("/<img[^>]+\>/i", "", nl2br(mb_strimwidth($oPost->body, 0, 800, '...'))); ?>
+              <br><br>
+              <?php require 'inc/control_buttons.php' ?>
+  					</div>
+  					<div class="col s12 m6 l4">
+  						<img src="<?=ROOT_URL?>static/img/posts/<?= $oPost->image ?>" class="materialboxed responsive-img" alt="<?= $oPost->title ?>"/>
+  						<br/><br/>
+  				  	<a class="btn light-blue waves-effect waves-light" href="<?=ROOT_URL?>?p=blog&amp;a=post&amp;id=<?=$oPost->id?>">Lire le chapitre</a>
+  					</div>
+  				</div>
+  			</div>
+  		</div>
+    <?php endforeach ?>
+  </div>
+</main>
+<?php require 'inc/footer.php' ?>
