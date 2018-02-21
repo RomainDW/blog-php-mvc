47ef7442ecb857b2df7b393ce228d7ef5fa454cb
RomainDW <romain.ollier34@gmail.com>
2018-02-21 11:09:22 +0100

    Stylisation de la page 404

--- View/not_found.php
+++ View/not_found.php
@@ -1,6 +1,21 @@
 <?php require 'inc/header.php' ?>
-<div class="container">
-  <h1>Page Not Found</h1>
 
-  <p>Whoops! Désolé mais cette page n'existe pas.</p>
+<style type="text/css">
+  div.vert-align {
+    position:absolute;
+    left: 50%;
+    top: 50%;
+    width: 600px;
+    height: 200px;
+    margin-left: -300px;
+    margin-top: -100px;
+}
+</style>
+
+<div class="center-align vert-align">
+  <h1>404 Not Found</h1>
+
+  <p>Le document / fichier demandé n'a pas été trouvé sur ce serveur</p>
+
+  <a href="<?=ROOT_URL?>?p=blog&amp;a=index" class="btn blue waves-effect waves-light">Retourner à l'accueil</a>
 </div>
